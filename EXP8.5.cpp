e32a6b5877b42410881013361a27cc7af9a18f14
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-02 10:10:33 +0530

    Create EXP8.5.cpp

---
+++ EXP8.5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+const int MAX = 100;
+
+void DiagonalSum(int mat[][MAX], int n) 
+{ 
+    int principal = 0;
+    
+    for (int i = 0; i < n; i++)  
+    { 
+        // Condition for principal diagonal 
+        principal += mat[i][i]; 
+    } 
+  
+    cout << "Sum of the diagonal elements is: " << principal << endl; 
+} 
+
+int main() 
+{ 
+    int a[][MAX] = {{1, 2, 3, 4},  
+                    {5, 6, 7, 8},  
+                    {1, 2, 3, 4},  
+                    {5, 6, 7, 8}}; 
+    DiagonalSum(a, 4); 
+return 0;
+}
