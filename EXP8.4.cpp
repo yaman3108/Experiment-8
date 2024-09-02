ecf51d034259965f2a4018ed065c79e61c3200b7
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-02 10:16:29 +0530

    Update EXP8.4.cpp

--- EXP8.4.cpp
+++ EXP8.4.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int r, c ;
 
-    // Getting the size of the matrix
+    // size of the matrix
     cout << "Enter the number of rows and columns of the matrix: ";
     cin >> r  >> c ;
 
     int m[r][c], transpose[c][r];
 
-    // Getting elements of the matrix
+    // elements of the matrix
     cout << "Enter elements of the first matrix:" << endl;
     for (int i = 0; i < r; ++i) {
         for (int j = 0; j < c; ++j) {
@@ -19,14 +19,14 @@ int main() {
         }
     }
 
-    // Transposing the matrix
+    // transpose of the matrix
     for(int i = 0; i < r; ++i) {
         for(int j = 0; j < c; ++j) {
             transpose[j][i] = m[i][j];
         }
     }
 
-    // Displaying the transpose of the matrix
+    // printing the transposed matrix
     cout << "\nTranspose of the matrix:" << endl;
     for(int i = 0; i < c; ++i) {
         for(int j = 0; j < r; ++j) {
