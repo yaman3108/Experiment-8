2bd445a3273d6b2b28bede089085c2472a335bb8
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-02 10:05:46 +0530

    Create EXP8.1.cpp

---
+++ EXP8.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int temp[3][3],i,j,k,l;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout<<"enter element- ("<<i<<j<<"): ";
+            cin>>temp[i][j];
+        }
+    }
+    for(k=0;k<3;k++)
+    {
+        for(l=0;l<3;l++)
+        {
+            cout<<temp[k][l];
+            cout<<" ";
+        }
+    }    
+    return 0;
+}
